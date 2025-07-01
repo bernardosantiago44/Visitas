cd730e03962fa562f33739fd13424ffb297153c4
Bernardo Santiago <bernardosantiago44@gmail.com>
2025-06-30 19:54:14 -0600

    Initial commit

---
+++ Pages/Customers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Visitas.Pages.Customers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
